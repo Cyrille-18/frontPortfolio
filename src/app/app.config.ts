a9ff5aa6f767fbd392e1e392c7cde22b6f9473ac
Cyrille-18 <cyrillepio@hotmail.com>
2026-05-30 16:52:30 +0000

    feat: add Taiga UI library and update app configuration
    - Added "taiga-ui" dependency to package.json.
    - Updated AppComponent to be standalone.
    - Enhanced app configuration to include Taiga UI modules and BrowserAnimationsModule for improved UI components.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,27 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideZoneChangeDetection,
+  importProvidersFrom,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  TuiRootModule,
+  TuiDialogModule,
+  TuiNotificationsModule,
+} from '@taiga-ui/core';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    importProvidersFrom(
+      BrowserAnimationsModule,
+      TuiRootModule,
+      TuiDialogModule,
+      TuiNotificationsModule,
+    ),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+  ],
 };
