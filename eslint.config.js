f0f809f8a1009b0425cfec427d4ab80dddd9c15b
Cyrille-18 <cyrillepio@hotmail.com>
2026-06-08 09:53:18 +0000

    chore: refactor ESLint configuration for improved TypeScript and Angular support

--- eslint.config.js
+++ eslint.config.js
@@ -1,33 +1,31 @@
-// @ts-check
-const eslint = require("@eslint/js");
-const { defineConfig } = require("eslint/config");
+// eslint.config.js
+const js = require("@eslint/js");
 const tseslint = require("typescript-eslint");
-const angular = require("angular-eslint");
 const jsdoc = require("eslint-plugin-jsdoc");
 const prettier = require("eslint-config-prettier");
+const angular = require("angular-eslint/configs");
 
-module.exports = defineConfig([
-  // files to ignore
+module.exports = tseslint.config(
+  // Ignorés
   {
-    ignores: ["node_modules/**", "dist/**", ".angular/**", "*.spec.ts"],
+    ignores: ["node_modules/**", "dist/**", ".angular/**", "**/*.spec.ts"],
   },
 
-  // TypeScript configs
+  // TypeScript + Angular
   {
     files: ["**/*.ts"],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
+      prettier,
+    ],
     plugins: {
       jsdoc,
     },
-    extends: [
-      eslint.configs.recommended,
-      tseslint.configs.recommended,
-      tseslint.configs.stylistic,
-      angular.configs.tsRecommended,
-      prettier,
-    ],
     processor: angular.processInlineTemplates,
     rules: {
-      // Angular
       "@angular-eslint/directive-selector": [
         "error",
         {
@@ -44,60 +42,36 @@ module.exports = defineConfig([
           style: "kebab-case",
         },
       ],
+
       "@angular-eslint/no-empty-lifecycle-method": "warn",
       "@angular-eslint/use-lifecycle-interface": "error",
 
-      // TypeScript
       "@typescript-eslint/no-explicit-any": "warn",
       "@typescript-eslint/no-unused-vars": [
         "warn",
         { argsIgnorePattern: "^_" },
       ],
-      "@typescript-eslint/explicit-function-return-type": [
-        "warn",
-        {
-          allowExpressions: true,
-          allowTypedFunctionExpressions: true,
-        },
-      ],
+      "@typescript-eslint/explicit-function-return-type": "warn",
       "@typescript-eslint/no-inferrable-types": "warn",
 
-      // Documentation JSDoc
-      "jsdoc/require-jsdoc": [
-        "warn",
-        {
-          require: {
-            FunctionDeclaration: true,
-            MethodDefinition: true,
-            ClassDeclaration: true,
-            ArrowFunctionExpression: false,
-          },
-        },
-      ],
-      "jsdoc/require-description": "warn",
-      "jsdoc/require-param": "warn",
-      "jsdoc/require-param-type": "off",
-      "jsdoc/require-returns": "warn",
-      "jsdoc/require-returns-type": "off",
+      "jsdoc/require-jsdoc": "off",
 
-      // General quality
       "no-console": "warn",
       "no-debugger": "error",
-      "no-duplicate-imports": "error",
       eqeqeq: ["error", "always"],
     },
   },
 
-  // HTML Config (templates Angular)
+  // HTML Angular templates
   {
     files: ["**/*.html"],
     extends: [
-      angular.configs.templateRecommended,
-      angular.configs.templateAccessibility,
+      ...angular.configs.templateRecommended,
+      ...angular.configs.templateAccessibility,
     ],
     rules: {
       "@angular-eslint/template/no-negated-async": "warn",
       "@angular-eslint/template/use-track-by-function": "warn",
     },
   },
-]);
+);
