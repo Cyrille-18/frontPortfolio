37a527f4f1b633791bc3a70d8eeda7bffbc39f4b
Cyrille-18 <cyrillepio@hotmail.com>
2026-06-29 09:08:35 +0000

    feat: refactor app component to integrate NavbarComponent and enhance layout
    - Added a new CSS style for the app shell to improve layout and background transition.
    - Updated app.component.html to include a main wrapper for the router outlet.
    - Modified app.component.ts to implement AfterViewInit and dynamically create the NavbarComponent.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
-import { TuiRoot } from "@taiga-ui/core";
-import { Component } from '@angular/core';
+import { TuiRoot } from '@taiga-ui/core';
+import {
+  AfterViewInit,
+  Component,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, TuiRoot],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'portfolio';
+  @ViewChild('navbarHost', { read: ViewContainerRef, static: true })
+  protected navbarHost!: ViewContainerRef;
+
+  public ngAfterViewInit(): void {
+    this.navbarHost.createComponent(NavbarComponent);
+  }
 }
