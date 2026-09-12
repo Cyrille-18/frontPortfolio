83019a5b9b7c3f67e3ea1413955a89a0783387db
Cyrille-18 <cyrillepio@hotmail.com>
2026-09-12 16:16:33 +0200

    feat: update app layout by integrating Navbar and Hero components, and adjust CSS for app-shell

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,16 @@
 import { TuiRoot } from '@taiga-ui/core';
-import {
-  AfterViewInit,
-  Component,
-  ViewChild,
-  ViewContainerRef,
-} from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { HeroComponent } from './features/hero/hero.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TuiRoot],
+  imports: [RouterOutlet, TuiRoot, NavbarComponent, HeroComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent {
   title = 'portfolio';
-  @ViewChild('navbarHost', { read: ViewContainerRef, static: true })
-  protected navbarHost!: ViewContainerRef;
-
-  public ngAfterViewInit(): void {
-    this.navbarHost.createComponent(NavbarComponent);
-  }
 }
